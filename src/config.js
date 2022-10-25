0597e8ca0ea688afa05a56db7c8d523ae12be42e
SeongsuPark <98324846+HolyWatter@users.noreply.github.com>
2022-10-26 02:40:06 +0900

    Delete config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL_SI = 'http://10.58.52.114:3000';
-export const BASE_URL_DY = 'http://10.58.52.108:3000';
-
-export const api = {
-  signup: `${BASE_URL_SI}/users/signup`,
-  signin: `${BASE_URL_SI}/users/signin`,
-  wishlists: `${BASE_URL_SI}/wishlists`,
-  products: `${BASE_URL_DY}/products`,
-};
